fd5bcf6c48322e01ce685d6176e11c742b56aac1
mojo9900 <77331850+mojo9900@users.noreply.github.com>
2021-06-15 22:11:41 +0800

    Add files via upload

---
+++ storage/framework/views/da44d96fbdaa880fa728c2657cedf9b081a1cd0c.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+   <div class="row" style="margin-top:45px">
+      <div class="col-md-4 col-md-offset-4">
+           <h4>Login | Sign In</h4><hr>
+           <form action="<?php echo e(route('auth.check')); ?>" method="post">
+            <?php if(Session::get('fail')): ?>
+               <div class="alert alert-danger">
+                  <?php echo e(Session::get('fail')); ?>
+
+               </div>
+            <?php endif; ?>
+  
+           <?php echo csrf_field(); ?>
+              <div class="form-group">
+                 <label>Email</label>
+                 <input type="text" class="form-control" name="email" placeholder="Enter email address" value="<?php echo e(old('email')); ?>">
+                 <span class="text-danger"><?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?><?php echo e($message); ?> <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?></span>
+              </div>
+              <div class="form-group">
+                 <label>Password</label>
+                 <input type="password" class="form-control" name="password" placeholder="Enter password">
+                 <span class="text-danger"><?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?><?php echo e($message); ?> <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?></span>
+              </div>
+              <button type="submit" class="btn btn-block btn-primary">Sign In</button>
+              <br>
+              <a href="<?php echo e(route('auth.register')); ?>">Dont have an account? Create new account</a>
+           </form>
+      </div>
+   </div>
+</div>
+    
+</body>
+</html><?php /**PATH C:\xampp\htdocs\Laravel\Laravel\resources\views/auth/login.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e05da46de02c6082f05abd4557c6675730bb4025.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+   <div class="row" style="margin-top:45px">
+      <div class="col-md-4 col-md-offset-4">
+           <h4>Register | Sign Up</h4><hr>
+           <form action="<?php echo e(route('auth.save')); ?>" method="post">
+
+           <?php if(Session::get('success')): ?>
+             <div class="alert alert-success">
+                <?php echo e(Session::get('success')); ?>
+
+             </div>
+           <?php endif; ?>
+
+           <?php if(Session::get('fail')): ?>
+             <div class="alert alert-danger">
+                <?php echo e(Session::get('fail')); ?>
+
+             </div>
+           <?php endif; ?>
+
+           <?php echo csrf_field(); ?>
+           <div class="form-group">
+                 <label>Name</label>
+                 <input type="text" class="form-control" name="name" placeholder="Enter full name" value="<?php echo e(old('name')); ?>">
+                 <span class="text-danger"><?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?><?php echo e($message); ?> <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?></span>
+              </div>
+              <div class="form-group">
+                 <label>Email</label>
+                 <input type="text" class="form-control" name="email" placeholder="Enter email address" value="<?php echo e(old('email')); ?>">
+                 <span class="text-danger"><?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?><?php echo e($message); ?> <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?></span>
+              </div>
+              <div class="form-group">
+                 <label>Password</label>
+                 <input type="password" class="form-control" name="password" placeholder="Enter password">
+                 <span class="text-danger"><?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?><?php echo e($message); ?> <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?></span>
+              </div>
+              <button type="submit" class="btn btn-block btn-primary">Sign Up</button>
+              <br>
+              <a href="<?php echo e(route('auth.login')); ?>">Already have an account? Sign In</a>
+           </form>
+      </div>
+   </div>
+</div>
+    
+</body>
+</html><?php /**PATH C:\xampp\htdocs\Laravel\Laravel\resources\views/auth/register.blade.php ENDPATH**/ ?>
